8f2a76d24eb06562246ec17d6f971cf6d926ee34
Deepu S <ss.deepu@gmail.com>
2025-08-02 00:43:35 +0000

    Remove horizontal scrolling by Reduce Column width of Description of Tra

--- src/components/dashboard/transactions/transaction-table.tsx
+++ src/components/dashboard/transactions/transaction-table.tsx
@@ -730,10 +730,10 @@ export function TransactionTable({
             <TableRow>
               <TableHead className="w-[5%]">Sl. No.</TableHead>
               <TableHead className="w-[10%]">Date</TableHead>
-              <TableHead className="w-[20%]">Description</TableHead>
+              <TableHead className="w-[18%]">Description</TableHead>
               <TableHead className="w-[8%]">Type</TableHead>
-              <TableHead className="w-[17%]">Account</TableHead>
-              <TableHead className="w-[15%]">Category</TableHead>
+              <TableHead className="w-[16%]">Account</TableHead>
+              <TableHead className="w-[18%]">Category</TableHead>
               <TableHead className="w-[10%] text-right">Credit</TableHead>
               <TableHead className="w-[10%] text-right">Debit</TableHead>
               <TableHead className="w-[5%] text-right print-hide">Actions</TableHead>
