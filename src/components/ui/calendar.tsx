bb76bc4e9299eef780b8208e68e9793413c2d4f9
Deepu S <ss.deepu@gmail.com>
2025-12-25 04:32:01 +0000

    npm run build
    > nextn@0.1.0 build
    > next build
    
       ▲ Next.js 15.4.8

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -55,8 +55,12 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        Chevron: ({ orientation }) =>
+          orientation === 'left' ? (
+            <ChevronLeft className="h-4 w-4" />
+          ) : (
+            <ChevronRight className="h-4 w-4" />
+          ),
         Day: (props: DayProps) => <DefaultDay {...props} />,
       }}
       {...props}
