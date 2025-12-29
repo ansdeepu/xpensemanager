a3f528d193654d8e2c1a492c9445ca8cd9de001f
Deepu S <ss.deepu@gmail.com>
2025-12-29 23:52:03 +0000

    In Dashboard, remove Financial Summary, Daily & Monthly Expense Overview

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,6 @@
 import { AccountBalances } from "@/components/dashboard/overview/account-balances";
 import { CategoryExpenses } from "@/components/dashboard/overview/category-expenses";
-import { MainStats } from "@/components/dashboard/overview/main-stats";
 import { NoticeBoard } from "@/components/dashboard/overview/notice-board";
-import { OverviewChart } from "@/components/dashboard/overview/overview-chart";
-import { RecentTransactions } from "@/components/dashboard/overview/recent-transactions";
 
 export default function DashboardPage() {
   return (
@@ -12,11 +9,6 @@ export default function DashboardPage() {
        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-7">
          <CategoryExpenses />
          <NoticeBoard />
-      </div>
-      <MainStats />
-      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-7">
-        <OverviewChart />
-        <RecentTransactions />
       </div>
     </div>
   );
