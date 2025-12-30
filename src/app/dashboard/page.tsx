d1d1cb2d505eff43a3ad1e68e7e0e9c1100ae25c
Deepu S <ss.deepu@gmail.com>
2025-12-30 00:11:02 +0000

    Remove Expense Distribution from Dashboard and retrieve my previous vers

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,18 +1,25 @@
 import { AccountBalances } from "@/components/dashboard/overview/account-balances";
 import { CategoryExpenses } from "@/components/dashboard/overview/category-expenses";
-import { ExpenseDistributionChart } from "@/components/dashboard/overview/expense-distribution-chart";
 import { NoticeBoard } from "@/components/dashboard/overview/notice-board";
+import { MainStats } from "@/components/dashboard/overview/main-stats";
+import { OverviewChart } from "@/components/dashboard/overview/overview-chart";
+import { RecentTransactions } from "@/components/dashboard/overview/recent-transactions";
+
 
 export default function DashboardPage() {
   return (
     <div className="flex flex-col gap-6">
       <AccountBalances />
+      <MainStats />
+      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-7">
+        <CategoryExpenses />
+        <div className="lg:col-span-3 flex flex-col gap-6">
+          <NoticeBoard />
+        </div>
+      </div>
        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-7">
-         <CategoryExpenses />
-         <div className="lg:col-span-3 flex flex-col gap-6">
-            <ExpenseDistributionChart />
-            <NoticeBoard />
-         </div>
+          <OverviewChart />
+          <RecentTransactions />
       </div>
     </div>
   );
