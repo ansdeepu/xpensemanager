893bc33bfe0d8c7dbc72360570849923cabb365b
Deepu S <ss.deepu@gmail.com>
2026-04-08 04:30:33 +0000

    In Primary (SBI) tab, arrange Bank Balance, Digital, Cash and SBI Credit

--- src/app/dashboard/transactions/page.tsx
+++ src/app/dashboard/transactions/page.tsx
@@ -91,7 +91,6 @@ export default function TransactionsPage(props: {
   params: Promise<any>;
   searchParams: Promise<any>;
 }) {
-  // Access dynamic APIs correctly for Next.js 15
   const params = use(props.params);
   const searchParams = use(props.searchParams);
 
@@ -593,7 +592,7 @@ export default function TransactionsPage(props: {
   return (
     <div className="space-y-6">
        <Tabs value={activeTab} onValueChange={setActiveTab}>
-          <TabsList className="flex w-full items-stretch justify-start flex-wrap h-auto p-1 bg-transparent gap-3">
+          <TabsList className="rounded-md text-muted-foreground flex w-full items-stretch justify-start overflow-x-auto h-auto p-1 bg-transparent gap-3">
             {primaryAccount && (
                 <TabsTrigger value={primaryAccount.id} asChild className="h-full p-0 min-w-[580px] flex-shrink-0">
                     <div className={cn("rounded-lg border-2 flex flex-col p-3 items-start text-left gap-2 cursor-pointer transition-shadow h-full w-full", activeTab === primaryAccount.id ? "shadow-lg border-primary bg-lime-100/50 dark:bg-lime-900/50" : "bg-card")}>
